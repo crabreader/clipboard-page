457d76c933aab56fc7abe22d3d7e145aeb3a1896
Alexander Doja <alexander.doja@outlook.com>
2024-03-26 17:50:19 +0100

    MutationObserver to intercept append child

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="container m-4">
+  <slot />
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.slate.800);
+    color: theme(colors.stone.200);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  function muationObs() {
+    const targetNode = document.body;
+
+    const config = { childList: true };
+
+    const callback = (mutationList: any, observer: any) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === "childList") {
+          mutation.addedNodes.forEach((node: Node) => {
+            if (node.nodeName === 'P') {
+              handleParagraph(node);
+            }
+          })
+        }
+      }
+    };
+
+    const observer = new MutationObserver(callback);
+
+    observer.observe(targetNode, config);
+  }
+
+  function handleParagraph(paragraph: Node) {
+    const targetElement = document.getElementById("target");
+
+    if (targetElement) {
+      targetElement.appendChild(paragraph); 
+      console.log("A child node has been added.");
+    }
+  }
+
+  onMount(() => {
+    muationObs();
+  });
+</script>
+
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="border-4 rounded-lg m-4 p-4" id="target">
+  <p>Example</p>
+</div>
