eaf049492e22cb4c5b19bd39c07d272b0450f7d0
Alexander Doja <alexander.doja@outlook.com>
2024-04-05 21:52:11 +0200

    store lines in array and count chars, options to delete lines

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,4 +3,4 @@
     export let onClick: () => void;
 </script>
 
-<button class="bg-slate-500 hover:bg-slate-700 active:ring-slate-400 text-white font-bold py-2 px-4 m-1 rounded ring" on:click={onClick}>{content}</button>
+<button class="bg-slate-500 hover:bg-slate-700 active:ring-slate-400 text-white font-bold px-2 m-1 rounded ring" on:click={onClick}>{content}</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,13 @@
   import { onMount } from "svelte";
   import Button from "../components/Button.svelte";
 
+  const CHARS_TO_IGNORE = "「」『』[]()〈〉≪≫。、.,'：！?！？…――─ｰ～→♪" + '"' + "　" + "  "
+  
   export let fontSize = 30;
   export let autoScroll = true;
+  export let lines: string[] = [];
+  export let chars = 0;
+
 
   function scrollToBottom() {
       window.scrollTo(0, document.body.scrollHeight);
@@ -23,8 +28,12 @@
               if (autoScroll) {
                 scrollToBottom();
               }
+
+              if (node.parentNode) {
+                node.parentNode.removeChild(node);
+              }
             }
-          })
+          });
         }
       }
     };
@@ -35,39 +44,92 @@
   }
 
   function handleParagraph(paragraph: Node) {
-    const targetElement = document.getElementById("target");
 
-    if (targetElement) {
-      const pText = "・" + paragraph.textContent;
-      paragraph.textContent = pText;
+    const pText = paragraph.textContent;
 
-      targetElement.appendChild(paragraph); 
+    if (pText) {
+      lines = [...lines, pText];
+      chars += countChars(pText);
+
+      localStorage.setItem("lines", JSON.stringify(lines));
     }
   }
 
+  function countChars(line: string) {
+    let filtered = "";
+
+    for (let i = 0; i < line.length; i++) {
+      let ignore = false;
+
+      for (let j = 0; j < CHARS_TO_IGNORE.length; j++) {
+        if (line[i] === CHARS_TO_IGNORE[j]) {
+          ignore = true;
+        }      
+      }
+
+      if (!ignore) {
+        filtered += line[i];
+      }
+    }
+
+    return filtered.length;
+  }
+
+  function toggleAutoscroll() {
+    autoScroll = !autoScroll;
+  }
+
   function increaseFontSize() {
-    fontSize++;
+    fontSize += 2;
   }
 
   function decreaseFontSize() {
-    fontSize--;
+    fontSize -=2;
+  }
+
+  function deleteLastLine() {
+    chars -= countChars(lines[lines.length - 1]);
+    lines = [...lines.slice(0, lines.length - 1)];
+    localStorage.setItem("lines", JSON.stringify(lines));
+  }
+
+  function deleteAllLines() {
+    lines = [];
+    localStorage.removeItem("lines");
   }
 
   onMount(() => {
     muationObs();
+
+    const localLines = localStorage.getItem("lines");
+    if (localLines) {
+      lines = JSON.parse(localLines);
+
+      lines.forEach(line => {
+        chars += countChars(line);
+      });
+    }
   });
 </script>
 
-<div class="p-4 sticky top-0 backdrop-blur-md hover:backdrop-blur-xl">
+<div class="p-4 sticky top-0 backdrop-blur-xl">
   <h1 class="text-2xl inline">Font Size: {fontSize}</h1>
 
   <Button content="-" onClick={decreaseFontSize} />
   <Button content="+" onClick={increaseFontSize} />
 
-  <h1 class="text-2xl inline">Autoscroll: </h1>
-  <input type="checkbox" id="myCheckbox" class="form-checkbox h-5 w-5 text-blue-600 border-gray-300 rounded focus:ring-blue-500" bind:checked={autoScroll}>
+  <Button content="Toggle autoscroll" onClick={toggleAutoscroll} />
+
+  <Button content="Delete all lines" onClick={deleteAllLines} />
+
+  <Button content="Delete last line" onClick={deleteLastLine} />
+
+  <h1 class="text-2xl inline">{lines.length} / {chars}</h1>
 </div>
 
 <div style="font-size: {fontSize}px;" class="border-4 rounded-lg m-4 p-4" id="target">
   <p>(´• ω •`)ﾉ</p>
+  {#each lines as line}
+    <p><span class="text-slate-400">・</span>{line}</p>
+  {/each}
 </div>
